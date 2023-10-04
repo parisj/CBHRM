02a9893364f6d21f8eba1399e4836cd22e4fce2b
Anton Paris <anton.j.paris@gmail.com>
2023-10-04 12:57:52 +0200

    refresh laptop

---
+++ time_signal.py
@@ -0,0 +1,16 @@
+class time_signal: 
+    
+    def __init__():
+        pass
+    
+    def update_sample():
+        pass
+    
+    def update_plot():
+        pass
+    
+    def processing():
+        pass
+    
+    def 
+    
